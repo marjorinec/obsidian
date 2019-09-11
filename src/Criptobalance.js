d0833ff7572196c28d9695c3033bdef9aecc019d
Alline Marjorie <allinemarjorie@gmail.com>
2019-09-10 22:27:06 -0300

    feat: get bitcoin value using axios

--- src/Criptobalance.js
+++ src/Criptobalance.js
@@ -1,25 +1,49 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Spinner } from 'react-bootstrap'
+import axios from 'axios'
 
 class Criptobalance extends React.Component {
   constructor(props) {
     super(props)
     this.coinCode = props.coinName === "Bitcoin" ? "BTC" : "BRT"
+    this.state = {
+      "bitcoinValue": "",
+      "dollarValue": "",
+      "ready": false
+    }
   }
-  render() {
 
-    return (
-      <Card style={{ width: '12rem'}}>
-        <Card.Body>
-          <Card.Title>
-            {this.props.coinName}
-          </Card.Title>
-          <Card.Text>
-            {this.coinCode} = X BRL
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    )
+  async getBitcoinValue() {
+    const bitcoinValue = await axios.get("https://www.mercadobitcoin.net/api/BTC/ticker/")
+    this.setState({"bitcoinValue": bitcoinValue.data.ticker.buy})
+  }
+
+  async componentDidMount(prevProps, prevState) {
+      await this.getBitcoinValue()
+      this.setState({"ready": true})
+  }
+
+  render() {
+    if (this.state.ready === false) {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      )
+    } else {
+      return (
+        <Card style={{ width: '12rem'}}>
+          <Card.Body>
+            <Card.Title>
+              {this.props.coinName}
+            </Card.Title>
+            <Card.Text>
+              {this.coinCode} = X BRL {this.state.bitcoinValue}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )
+    }
   }
 }
 
