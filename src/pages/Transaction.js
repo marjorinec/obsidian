5c291f0711ed7a56e2637137e593fc0a1e80b77b
Alline Marjorie <allinemarjorie@gmail.com>
2019-09-15 20:56:12 -0300

    chore: add currency selector

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,12 +1,44 @@
 import React from 'react'
+import { Container, Form, Row, Col } from 'react-bootstrap'
+import Currencies from './../Currencies.json'
 
 class Transaction extends React.Component {
 
+  renderCoinSelector(varName) {
+    const currencyList = Currencies.map(({ name, code }) => {
+      return (
+        <Col key={code}>
+          <Form.Check
+            type="radio"
+            name={varName}
+            value={code}
+            label={name}
+          />
+        </Col>
+      )
+    })
+
+    return (
+      <Row>
+        {currencyList}
+      </Row>
+    )
+  }
+
   render() {
     return (
-      <section>
-        aaaa transação
-      </section>
+      <Container>
+        <Form>
+          <section className="source">
+            <h3>Moeda origem</h3>
+            {this.renderCoinSelector('source')}
+          </section>
+          <section className="destination">
+            <h3>Moeda destino</h3>
+            {this.renderCoinSelector('destination')}
+          </section>                  
+        </Form>
+      </Container>
     )
   }
 }
