8d7298022b8b0e2afe7a0adcfbbb2f8c12c5c4cb
Alline Marjorie <allinemarjorie@gmail.com>
2019-09-17 09:15:34 -0300

    fix: <ul> nesting inside <p> error

--- src/Balance.js
+++ src/Balance.js
@@ -9,12 +9,10 @@ class Balance extends React.Component {
           <Card.Title>
             Seu Saldo
           </Card.Title>
-          <Card.Text>
-            <ul className="list-unstyled">
-              <li>R$ 100.000,00</li>
-              <li>BRT 15</li>
-              <li> Bitcoin 20</li>
-            </ul>
+          <Card.Text as="ul" className="list-unstyled">
+            <li>R$ 100.000,00</li>
+            <li>BRT 15</li>
+            <li> Bitcoin 20</li>
           </Card.Text>
         </Card.Body>
       </Card>
