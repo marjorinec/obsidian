11e41bb47afe5e853c5721268f43ce24a1e52a9c
Alline Marjorie <allinemarjorie@gmail.com>
2019-09-17 09:17:56 -0300

    chore: wrap Redux on root component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import Store from './store/main'
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
