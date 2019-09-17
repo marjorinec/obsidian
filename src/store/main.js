ee803524c7aa98baaaa5a62ce5f0ffa9203485f7
Alline Marjorie <allinemarjorie@gmail.com>
2019-09-17 08:46:30 -0300

    chore: support Redux DevTools extension

--- src/store/main.js
+++ src/store/main.js
@@ -4,7 +4,8 @@ import reducer from './reducer'
 
 const store = createStore(
   reducer,
-  initialState
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
