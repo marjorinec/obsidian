1071d7918e0025bbd13d3c5063dbb5a3391285c8
Alline Marjorie <allinemarjorie@gmail.com>
2019-09-19 23:33:50 -0300

    chore: display transaction confirmation & runs it

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Container, Form, Row, Col, InputGroup, Button, Card } from 'react-bootstrap'
+import { Container, Form, Row, Col, InputGroup, Button, Card, Alert } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import Currencies from './../Currencies.json'
+
+import { increaseBalance, decreaseBalance, registerTransaction } from './../store/actions'
 
 class Transaction extends React.Component {
 
@@ -13,25 +15,60 @@ class Transaction extends React.Component {
       'source': null,
       'destination': null,
       'value': 0,
-      'convertedValue': null
+      'convertedValue': null,
+      'waitingConfirmation': false
     }
 
     this.handleCoinSelection = this.handleCoinSelection.bind(this)
     this.handleValueInput = this.handleValueInput.bind(this)
+    this.handleNext = this.handleNext.bind(this)
+    this.handleConfirm = this.handleConfirm.bind(this)
   }
 
   handleCoinSelection({target}) {
-    this.setState({ [target.name]: target.value })
+    this.setState({ 
+      [target.name]: target.value,
+      waitingConfirmation: false 
+    })
   }
 
   handleValueInput({target}) {
-    this.setState({ value: target.value })
+    this.setState({ 
+      value: target.value, 
+      waitingConfirmation: false 
+    })
     const convertedValue = this.convert(
       this.state.source,
       this.state.destination,
       target.value
     )
     this.setState({ convertedValue: convertedValue })
+  }
+
+  handleNext() {
+    this.setState({ waitingConfirmation: true })
+  }
+
+  handleConfirm() {
+    this.doTransaction()
+    this.props.history.push('/statement')
+  }
+
+  doTransaction(){
+    this.props.decreaseBalance(
+      this.state.source, this.state.value
+    )
+
+    this.props.increaseBalance(
+      this.destination, this.state.convertedValue
+    )
+
+    this.props.registerTransaction(
+      this.state.source,
+      this.state.destination,
+      this.state.value,
+      this.state.convertedValue
+    )
   }
 
   convert(source, destination, value) {
@@ -88,14 +125,13 @@ class Transaction extends React.Component {
     return `1 ${this.state.source} = ${this.convert(this.state.source, this.state.destination, 1).toFixed(6)} ${this.state.destination}`
   }
 
-  renderConvertedValue() {
+  renderConvertedValue(prefix="") {
     if (this.state.convertedValue) {
-      return `= ${this.state.convertedValue.toFixed(6)} ${this.state.destination}`
+      return `${prefix} ${this.state.convertedValue.toFixed(6)} ${this.state.destination}`
     } else {
       return 'Digite um valor para o ver convertido'
     }
   }
-
 
   renderValueSection() {
     if (this.state.source && this.state.destination) {
@@ -124,13 +160,39 @@ class Transaction extends React.Component {
             
           </Card.Body>
           <Card.Footer className="text-right">
-            <Button variant="primary" type="button">
+            <Button variant="primary" type="button" onClick={this.handleNext}>
               Converter
             </Button>
           </Card.Footer>
         </Card>  
       )
     }
+  }
+
+  renderConfirmation() {
+    return (
+      <Alert show={this.state.waitingConfirmation} variant="warning" className="my-3">
+        <Alert.Heading>
+          Confirme operação
+        </Alert.Heading>
+        <p>
+          Tem certeza que deseja converter 
+          <strong>
+            {` ${this.state.value} ${this.state.source} `}
+          </strong>
+          em
+          <strong>
+            {` ${this.renderConvertedValue()} `}?
+          </strong>
+        </p>
+        <hr/>
+        <div className="actions d-flex justify-content-end">
+          <Button variant="danger" onClick={this.handleConfirm}>
+            Confirmar
+          </Button>
+        </div>
+      </Alert>
+    )
   }
 
   render() {
@@ -145,7 +207,8 @@ class Transaction extends React.Component {
             <Card.Header as="h5">Selecione a Moeda de Destino</Card.Header>
             {this.renderCoinSelector('destination', this.state.source)}
           </Card>
-          {this.renderValueSection()}                
+          {this.renderValueSection()}
+          {this.renderConfirmation()}
         </Form>
       </Container>
     )
@@ -158,6 +221,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
+  decreaseBalance, increaseBalance, registerTransaction
 }, dispatch)
 
 export default connect(
